170774e6943fcb80471349a881591447cd6adc58
lbias <306508354@qq.com>
2017-06-03 23:58:44 +0800

    @reservation.user = current_user in controller api::v1::reservations#create

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ReservationsController < ApiController
                                     :seat_number => params[:seat_number],
                                     :customer_name => params[:customer_name],
                                     :customer_phone => params[:customer_phone] )
-
+    @reservation.user = current_user
     if @reservation.save
       render :json => { :booking_code => @reservation.booking_code,
                         :reservation_url => api_v1_reservation_url(@reservation.booking_code) }
@@ -36,5 +36,5 @@ class Api::V1::ReservationsController < ApiController
     @reservation = Reservation.find_by_booking_code!( params[:booking_code] )
     @reservation.destroy
     render :json => { :message => "已取消定位" }
-  end  
+  end
 end
